2c7538e63b6e9d91241172f3d901b39f086b19f3
Thalisson Felipe <thalissonlipe@gmail.com>
2020-05-19 19:31:42 -0300

    Runes unit tests refactored..

--- tests/test_blueprints_runes_blueprint.py
+++ tests/test_blueprints_runes_blueprint.py
@@ -4,116 +4,170 @@ from unittest.mock import patch, Mock
 from blueprints import runes_blueprint as rb
 
 
-MOCK_RUNES = [
-    {
-        'name': 'Vine Rune',
-        'image': 'path_to_image',
-        'biome': 'Promenade of the Condemned',
-        'location': 'In a room accessed by entering a door' +
-        'found at the base of a large overhang',
-        'enemy': 'Undead Archer',
-        'ability': 'Ability to sprout vines from special green blobs.',
-        'access': [
-            'Toxic Sewers',
-            'Ramparts',
-            'Dilapidated Arboretum (when paired with Teleportation Rune)'
-        ]
-    }
-]
+MOCK_RUNES = {
+    'name': 'runes',
+    'data': [
+        {
+            'name': 'Vine Rune',
+            'image': 'path_to_image',
+            'biome': 'Promenade of the Condemned',
+            'location': 'In a room accessed by entering a door' +
+            'found at the base of a large overhang',
+            'enemy': 'Undead Archer',
+            'ability': 'Ability to sprout vines from special green blobs.',
+            'access': [
+                'Toxic Sewers',
+                'Ramparts',
+                'Dilapidated Arboretum (when paired with Teleportation Rune)'
+            ]
+        }
+    ]
+}
 
 
 class test_blueprint_get_runes(unittest.TestCase):
 
     @patch(
-        'scrapper.Scrapper.get_runes',
-        return_value=MOCK_RUNES
-    )
-    @patch(
-        'json.dumps',
-        return_value=json.dumps(MOCK_RUNES)
+        'blueprints.runes_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_runes_success(
         self,
-        mock_json,
-        mock_scrapper
+        mock_current_app
     ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_runes = Mock(
+            return_value=MOCK_RUNES
+        )
+        mock_current_app.scrapper_manager.insert = Mock()
+
         ret = rb.get_runes()
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_runes.assert_called()
+        mock_current_app.scrapper_manager.insert.assert_called()
         self.assertEqual(ret.status_code, 200)
-        self.assertEqual(ret.data.decode(), json.dumps(MOCK_RUNES))
-        mock_json.assert_called()
-        mock_scrapper.assert_called()
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_RUNES['data']))
 
     @patch(
         'blueprints.runes_blueprint.current_app',
         return_value=Mock()
     )
+    def test_get_runes_success_from_memory(
+        self,
+        mock_current_app
+    ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=MOCK_RUNES)
+
+        ret = rb.get_runes()
+        mock_current_app.scrapper_manager.get.assert_called()
+        self.assertEqual(ret.status_code, 200)
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_RUNES['data']))
+
     @patch(
-        'scrapper.Scrapper.get_runes',
-        side_effect=Exception('Exception')
+        'blueprints.runes_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_runes_internal_error(
         self,
-        mock_scrapper,
         mock_current_app
     ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_runes = Mock(
+            side_effect=Exception()
+        )
         mock_current_app.logger.exception = Mock()
+
         ret = rb.get_runes()
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_runes.assert_called()
+        mock_current_app.logger.exception.assert_called()
         self.assertEqual(ret.status_code, 500)
         self.assertEqual(ret.data.decode(), 'Internal Server Error.')
-        mock_scrapper.assert_called()
-        mock_current_app.logger.exception.assert_called()
 
 
 class test_blueprint_get_rune_by_name(unittest.TestCase):
 
     @patch(
-        'json.dumps',
-        return_value=json.dumps(MOCK_RUNES[0])
-    )
-    @patch(
-        'scrapper.Scrapper.get_runes',
-        return_value=MOCK_RUNES
+        'blueprints.runes_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_rune_by_name_success(
         self,
-        mock_scrapper,
-        mock_json,
+        mock_current_app
     ):
-        ret = rb.get_rune_by_name(MOCK_RUNES[0]['name'])
-        self.assertEqual(ret.status_code, 200)
-        self.assertEqual(ret.data.decode(), json.dumps(MOCK_RUNES[0]))
-        mock_scrapper.assert_called()
-        mock_json.assert_called()
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_runes = Mock(
+            return_value=MOCK_RUNES
+        )
+        mock_current_app.scrapper_manager.insert = Mock()
 
-    @patch(
-        'scrapper.Scrapper.get_runes',
-        return_value=MOCK_RUNES
-    )
-    def test_get_rune_by_name_not_found(
-        self,
-        mock_scrapper
-    ):
-        ret = rb.get_rune_by_name(None)
-        self.assertEqual(ret.status_code, 404)
-        self.assertEqual(ret.data.decode(), 'Rune not found.')
-        mock_scrapper.assert_called()
+        ret = rb.get_rune_by_name(MOCK_RUNES['data'][0]['name'])
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_runes.assert_called()
+        mock_current_app.scrapper_manager.insert.assert_called()
+        self.assertEqual(ret.status_code, 200)
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_RUNES['data'][0]))
 
     @patch(
         'blueprints.runes_blueprint.current_app',
         return_value=Mock()
     )
+    def test_get_rune_by_name_success_from_memory(
+        self,
+        mock_current_app
+    ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=MOCK_RUNES)
+
+        ret = rb.get_rune_by_name(MOCK_RUNES['data'][0]['name'])
+        mock_current_app.scrapper_manager.get.assert_called()
+        self.assertEqual(ret.status_code, 200)
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_RUNES['data'][0]))
+
     @patch(
-        'scrapper.Scrapper.get_runes',
-        side_effect=Exception('Exception')
+        'blueprints.runes_blueprint.current_app',
+        return_value=Mock()
+    )
+    def test_get_rune_by_name_not_found(
+        self,
+        mock_current_app
+    ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_runes = Mock(
+            return_value=MOCK_RUNES
+        )
+        mock_current_app.scrapper_manager.insert = Mock()
+
+        ret = rb.get_rune_by_name(None)
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_runes.assert_called()
+        mock_current_app.scrapper_manager.insert.assert_called()
+        self.assertEqual(ret.status_code, 404)
+        self.assertEqual(ret.data.decode(), 'Rune not found.')
+
+    @patch(
+        'blueprints.runes_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_rune_by_name_internal_error(
         self,
-        mock_scrapper,
         mock_current_app
     ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_runes = Mock(
+            side_effect=Exception()
+        )
         mock_current_app.logger.exception = Mock()
-        ret = rb.get_rune_by_name(None)
+
+        ret = rb.get_rune_by_name(MOCK_RUNES['data'][0]['name'])
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_runes.assert_called()
+        mock_current_app.logger.exception.assert_called()
         self.assertEqual(ret.status_code, 500)
         self.assertEqual(ret.data.decode(), 'Internal Server Error.')
-        mock_scrapper.assert_called()
-        mock_current_app.logger.exception.assert_called()
