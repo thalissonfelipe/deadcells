0cc1063c59cfe3e6312a9e3bd3a05f518361fad1
Thalisson Felipe <thalissonlipe@gmail.com>
2020-05-19 19:46:21 -0300

    Gear unit tests refactored.

--- tests/test_blueprints_gears_blueprint.py
+++ tests/test_blueprints_gears_blueprint.py
@@ -4,86 +4,129 @@ from unittest.mock import patch, Mock
 from blueprints import gears_blueprint as gb
 
 
-MOCK_GEARS = [
-    {
-        'type': 'malee',
-        'gears': [
-            {
-                'image': 'path_to_image',
-                'name': 'Rusty Sword',
-                'description': 'Kills things. Sometimes...',
-                'location': 'Starter melee weapon until ...',
-                'base_dps': '119 DPS',
-                'special_dps': 'N/A',
-                'scaling': 'path_to_image'
-            }
-        ]
-    }
-]
+MOCK_GEARS = {
+    'name': 'gears',
+    'data': [
+        {
+            'type': 'malee_weapons',
+            'gears': [
+                {
+                    'image': 'path_to_image',
+                    'name': 'Rusty Sword',
+                    'description': 'Kills things. Sometimes...',
+                    'location': 'Starter melee weapon until ...',
+                    'base_dps': '119 DPS',
+                    'special_dps': 'N/A',
+                    'scaling': 'path_to_image'
+                }
+            ]
+        }
+    ]
+}
 
 
 class test_blueprint_get_gears(unittest.TestCase):
 
     @patch(
-        'scrapper.Scrapper.get_gears',
-        return_value=MOCK_GEARS
-    )
-    @patch(
-        'json.dumps',
-        return_value=json.dumps(MOCK_GEARS)
+        'blueprints.gears_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_gears_success(
         self,
-        mock_json,
-        mock_scrapper
+        mock_current_app
     ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_gears = Mock(
+            return_value=MOCK_GEARS
+        )
+        mock_current_app.scrapper_manager.insert = Mock()
+
         ret = gb.get_gears()
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_gears.assert_called()
+        mock_current_app.scrapper_manager.insert.assert_called()
         self.assertEqual(ret.status_code, 200)
-        self.assertEqual(ret.data.decode(), json.dumps(MOCK_GEARS))
-        mock_json.assert_called()
-        mock_scrapper.assert_called()
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_GEARS['data']))
 
     @patch(
         'blueprints.gears_blueprint.current_app',
         return_value=Mock()
     )
+    def test_get_gears_success_from_memory(
+        self,
+        mock_current_app
+    ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=MOCK_GEARS)
+
+        ret = gb.get_gears()
+        mock_current_app.scrapper_manager.get.assert_called()
+        self.assertEqual(ret.status_code, 200)
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_GEARS['data']))
+
     @patch(
-        'scrapper.Scrapper.get_gears',
-        side_effect=Exception('Exception')
+        'blueprints.gears_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_gears_internal_error(
         self,
-        mock_scrapper,
         mock_current_app
     ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_gears = Mock(
+            side_effect=Exception()
+        )
         mock_current_app.logger.exception = Mock()
+
         ret = gb.get_gears()
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_gears.assert_called()
+        mock_current_app.logger.exception.assert_called()
         self.assertEqual(ret.status_code, 500)
         self.assertEqual(ret.data.decode(), 'Internal Server Error.')
-        mock_scrapper.assert_called()
-        mock_current_app.logger.exception.assert_called()
 
 
 class test_blueprint_get_gears_by_type(unittest.TestCase):
 
     @patch(
-        'scrapper.Scrapper.get_gears',
-        return_value=MOCK_GEARS
-    )
-    @patch(
-        'json.dumps',
-        return_value=json.dumps(MOCK_GEARS)
+        'blueprints.gears_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_gears_by_type_success(
         self,
-        mock_json,
-        mock_scrapper
+        mock_current_app
     ):
-        ret = gb.get_gears_by_type('malee')
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_gears = Mock(
+            return_value=MOCK_GEARS
+        )
+        mock_current_app.scrapper_manager.insert = Mock()
+
+        ret = gb.get_gears_by_type('malee_weapons')
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_gears.assert_called()
+        mock_current_app.scrapper_manager.insert.assert_called()
         self.assertEqual(ret.status_code, 200)
-        self.assertEqual(ret.data.decode(), json.dumps(MOCK_GEARS[0]))
-        mock_json.assert_called()
-        mock_scrapper.assert_called()
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_GEARS['data'][0]))
+
+    @patch(
+        'blueprints.gears_blueprint.current_app',
+        return_value=Mock()
+    )
+    def test_get_gears_by_type_success_from_memory(
+        self,
+        mock_current_app
+    ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=MOCK_GEARS)
+
+        ret = gb.get_gears_by_type('malee_weapons')
+        mock_current_app.scrapper_manager.get.assert_called()
+        self.assertEqual(ret.status_code, 200)
+        self.assertEqual(ret.data.decode(), json.dumps(MOCK_GEARS['data'][0]))
 
     def test_get_gears_by_type_invalid_gear_type(self):
         ret = gb.get_gears_by_type(None)
@@ -94,53 +137,90 @@ class test_blueprint_get_gears_by_type(unittest.TestCase):
         'blueprints.gears_blueprint.current_app',
         return_value=Mock()
     )
-    @patch(
-        'scrapper.Scrapper.get_gears',
-        side_effect=Exception('Exception')
-    )
     def test_get_gears_by_type_internal_error(
         self,
-        mock_scrapper,
         mock_current_app
     ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_gears = Mock(
+            side_effect=Exception()
+        )
         mock_current_app.logger.exception = Mock()
-        ret = gb.get_gears_by_type('malee')
+
+        ret = gb.get_gears_by_type('malee_weapons')
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_gears.assert_called()
+        mock_current_app.logger.exception.assert_called()
         self.assertEqual(ret.status_code, 500)
         self.assertEqual(ret.data.decode(), 'Internal Server Error.')
-        mock_scrapper.assert_called()
-        mock_current_app.logger.exception.assert_called()
 
 
 class test_blueprint_get_gear_by_name(unittest.TestCase):
 
     @patch(
-        'scrapper.Scrapper.get_gears',
-        return_value=MOCK_GEARS
-    )
-    @patch(
-        'json.dumps',
-        return_value=json.dumps(MOCK_GEARS)
+        'blueprints.gears_blueprint.current_app',
+        return_value=Mock()
     )
     def test_get_gear_by_name_success(
         self,
-        mock_json,
-        mock_scrapper
+        mock_current_app
     ):
-        ret = gb.get_gear_by_name(MOCK_GEARS[0]['gears'][0]['name'])
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_gears = Mock(
+            return_value=MOCK_GEARS
+        )
+        mock_current_app.scrapper_manager.insert = Mock()
+
+        ret = gb.get_gear_by_name(MOCK_GEARS['data'][0]['gears'][0]['name'])
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_gears.assert_called()
+        mock_current_app.scrapper_manager.insert.assert_called()
         self.assertEqual(ret.status_code, 200)
         self.assertEqual(
             ret.data.decode(),
-            json.dumps(MOCK_GEARS[0]['gears'][0])
+            json.dumps(MOCK_GEARS['data'][0]['gears'][0])
         )
-        mock_json.assert_called()
-        mock_scrapper.assert_called()
 
     @patch(
-        'scrapper.Scrapper.get_gears',
-        return_value=MOCK_GEARS
+        'blueprints.gears_blueprint.current_app',
+        return_value=Mock()
     )
-    def test_get_gear_by_name_not_found(self, mock_scrapper):
+    def test_get_gear_by_name_success_from_memory(
+        self,
+        mock_current_app
+    ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=MOCK_GEARS)
+
+        ret = gb.get_gear_by_name(MOCK_GEARS['data'][0]['gears'][0]['name'])
+        mock_current_app.scrapper_manager.get.assert_called()
+        self.assertEqual(ret.status_code, 200)
+        self.assertEqual(
+            ret.data.decode(),
+            json.dumps(MOCK_GEARS['data'][0]['gears'][0])
+        )
+
+    @patch(
+        'blueprints.gears_blueprint.current_app',
+        return_value=Mock()
+    )
+    def test_get_gear_by_name_not_found(
+        self,
+        mock_current_app
+    ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_gears = Mock(
+            return_value=MOCK_GEARS
+        )
+        mock_current_app.scrapper_manager.insert = Mock()
+
         ret = gb.get_gear_by_name(None)
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_gears.assert_called()
+        mock_current_app.scrapper_manager.insert.assert_called()
         self.assertEqual(ret.status_code, 404)
         self.assertEqual(ret.data.decode(), 'Gear not found.')
 
@@ -148,18 +228,20 @@ class test_blueprint_get_gear_by_name(unittest.TestCase):
         'blueprints.gears_blueprint.current_app',
         return_value=Mock()
     )
-    @patch(
-        'scrapper.Scrapper.get_gears',
-        side_effect=Exception('Exception')
-    )
     def test_get_gear_by_name_internal_error(
         self,
-        mock_scrapper,
         mock_current_app
     ):
+        mock_current_app.scrapper_manager = Mock()
+        mock_current_app.scrapper_manager.get = Mock(return_value=None)
+        mock_current_app.scrapper_manager.get_gears = Mock(
+            side_effect=Exception()
+        )
         mock_current_app.logger.exception = Mock()
-        ret = gb.get_gear_by_name(MOCK_GEARS[0]['gears'][0]['name'])
+
+        ret = gb.get_gear_by_name(MOCK_GEARS['data'][0]['gears'][0]['name'])
+        mock_current_app.scrapper_manager.get.assert_called()
+        mock_current_app.scrapper_manager.get_gears.assert_called()
+        mock_current_app.logger.exception.assert_called()
         self.assertEqual(ret.status_code, 500)
         self.assertEqual(ret.data.decode(), 'Internal Server Error.')
-        mock_scrapper.assert_called()
-        mock_current_app.logger.exception.assert_called()
